47503229f9c2adc7597ccf8f2f182f6fc7aaaea4
ilayda <ilaydaozken@gmail.com>
2023-08-09 11:51:21 +0300

    Authentication

--- IdentityServer/CasgemMicroservice.IdentityServer/Controllers/UsersController.cs
+++ IdentityServer/CasgemMicroservice.IdentityServer/Controllers/UsersController.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CasgemMicroservice.IdentityServer.Dto;
+using CasgemMicroservice.IdentityServer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
 
 namespace CasgemMicroservice.IdentityServer.Controllers
 {
+    [Authorize(LocalApi.PolicyName)]
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> SingUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City,
+                NameSurname = signupDto.NameSurne
+            };
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+           return NoContent();
+        }
     }
 }
