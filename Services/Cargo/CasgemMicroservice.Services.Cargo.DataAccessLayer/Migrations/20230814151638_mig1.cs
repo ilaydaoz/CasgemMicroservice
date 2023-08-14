505d6044181f26d7050e7c9e009d8b7e31582c6d
ilayda <ilaydaozken@gmail.com>
2023-08-14 18:17:53 +0300

    cargo db bağlantısı

---
+++ Services/Cargo/CasgemMicroservice.Services.Cargo.DataAccessLayer/Migrations/20230814151638_mig1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CasgemMicroservice.Services.Cargo.DataAccessLayer.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CargoStates",
+                columns: table => new
+                {
+                    CargoStateID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CargoStates", x => x.CargoStateID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CargoDetails",
+                columns: table => new
+                {
+                    CargoDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    CargoStateId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CargoStatesCargoStateID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CargoDetails", x => x.CargoDetailId);
+                    table.ForeignKey(
+                        name: "FK_CargoDetails_CargoStates_CargoStatesCargoStateID",
+                        column: x => x.CargoStatesCargoStateID,
+                        principalTable: "CargoStates",
+                        principalColumn: "CargoStateID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CargoDetails_CargoStatesCargoStateID",
+                table: "CargoDetails",
+                column: "CargoStatesCargoStateID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CargoDetails");
+
+            migrationBuilder.DropTable(
+                name: "CargoStates");
+        }
+    }
+}
---
+++ Services/Cargo/CasgemMicroservice.Services.Cargo.EntityLayer/Entities/CargoDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemMicroservice.Services.Cargo.EntityLayer.Entities
+{
+    public class CargoDetail
+    {
+        public int CargoDetailId { get; set; }
+        public int OrderId { get; set; }
+        public string CargoStateId { get; set; }
+        public CargoState CargoStates { get; set; }
+    }
+}
